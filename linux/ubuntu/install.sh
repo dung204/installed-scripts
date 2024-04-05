487fd9a2ba180204bc402e8a869d8c13c664fcf7
dung204 <acezombiev4@gmail.com>
2024-04-05 08:51:44 -0700

    Add user to Docker group

--- linux/ubuntu/install.sh
+++ linux/ubuntu/install.sh
@@ -193,6 +193,7 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc > /dev/null 2>&1
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null 2>&1
 sudo apt-get update > /dev/null 2>&1
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y > /dev/null 2>&1
+sudo gpasswd -a $USER docker > /dev/null 2>&1
 
 # Check Docker Engine is installed
 docker --version > /dev/null 2>&1
